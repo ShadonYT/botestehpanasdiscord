548603e80718749914dfef2e6ad0e4106d1d0c65
ShadonYT <79522160+ShadonYT@users.noreply.github.com>
2021-02-26 07:09:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://trail-observant-soda.glitch.me"]
+const urls = ["https://ultra-tricky-mockingbird.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
